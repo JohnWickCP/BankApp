92789c9b6e6520d1fa7f1f4f0a7c41400b80dd40
JohnWickCP <phonk63utc@gmail.com>
2024-04-18 22:23:50 +0700

    feat : add ClientMenu interface

---
+++ src/main/java/CHBank/Controller/Client/ClientMenuController.java
@@ -0,0 +1,13 @@
+package CHBank.Controller.Client;
+
+import javafx.scene.control.Button;
+
+public class ClientMenuController {
+    public Button dashboard_but;
+    public Button transaction_but;
+    public Button acc_but;
+    public Button profile_but;
+    public Button logout_but;
+    public Button report_but;
+}
+
