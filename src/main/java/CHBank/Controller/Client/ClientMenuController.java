1990cd133648568fc0ffcffb9803a141d5680e9b
JohnWickCP <phonk63utc@gmail.com>
2024-04-19 21:34:57 +0700

    feat : add Ini interface

--- src/main/java/CHBank/Controller/Client/ClientMenuController.java
+++ src/main/java/CHBank/Controller/Client/ClientMenuController.java
@@ -1,13 +1,24 @@
 package CHBank.Controller.Client;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-public class ClientMenuController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientMenuController implements Initializable {
     public Button dashboard_but;
     public Button transaction_but;
     public Button acc_but;
     public Button profile_but;
     public Button logout_but;
     public Button report_but;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
+
 
