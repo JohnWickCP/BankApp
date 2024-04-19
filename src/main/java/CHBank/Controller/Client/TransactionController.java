d4e4608c6caaaf7b037bb57a04037c987889bc37
JohnWickCP <phonk63utc@gmail.com>
2024-04-20 00:32:08 +0700

    feat:add base Transaction

---
+++ src/main/java/CHBank/Controller/Client/TransactionController.java
@@ -0,0 +1,16 @@
+package CHBank.Controller.Client;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TransactionController implements Initializable {
+    public AnchorPane transaction_listview;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
