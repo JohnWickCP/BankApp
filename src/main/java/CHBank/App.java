62b93aa5d0b7d8a7e405278a336f655e58ab80d1
JohnWickCP <phonk63utc@gmail.com>
2024-04-19 23:34:53 +0700

    feat

--- src/main/java/CHBank/App.java
+++ src/main/java/CHBank/App.java
@@ -1,20 +1,14 @@
 package CHBank;
 
+import CHBank.Models.Model;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.util.Objects;
+
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        Scene scene = new Scene(fxmlLoader.load(Objects.requireNonNull(getClass().getResource("/Fxml/Login.fxml")).openStream()));
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/Styles/login.css")).toExternalForm());
-        stage.setScene(scene);
-        stage.setTitle("CHBank");
-        stage.show();
+        Model.getInstance().getView().showLoginWindow();
     }
 }
