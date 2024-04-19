f0eaff7212efb372a21d30dc59682ca12f7c2508
JohnWickCP <phonk63utc@gmail.com>
2024-04-19 23:34:00 +0700

    feat: add setOnaction to a button

--- src/main/java/CHBank/Controller/LoginController.java
+++ src/main/java/CHBank/Controller/LoginController.java
@@ -1,11 +1,16 @@
 package CHBank.Controller;
 
+import CHBank.Models.Model;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     public ChoiceBox acc_selector;
     public Label payee_add_label;
     public TextField payee_add_field;
@@ -13,4 +18,10 @@ public class LoginController {
     public TextField pass_add_field;
     public Button login_button;
     public Label error_label;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        login_button.setOnAction(event -> Model.getInstance().getView().showClientWindow());
+    }
 }
