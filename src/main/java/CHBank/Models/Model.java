9abd3302c768824e710ee706c5b7f22110f88d8e
JohnWickCP <phonk63utc@gmail.com>
2024-04-19 23:34:28 +0700

    feat

--- src/main/java/CHBank/Models/Model.java
+++ src/main/java/CHBank/Models/Model.java
@@ -1,4 +1,24 @@
 package CHBank.Models;
 
+import CHBank.Views.View;
+
 public class Model {
+    private static Model model;
+    private final View view;
+
+    private Model(){
+        this.view = new View();
+    }
+
+    public static synchronized Model getInstance(){
+        if(model == null){
+            model = new Model();
+        }
+        return model;
+    }
+
+    public View getView(){
+        return view;
+    }
+
 }
