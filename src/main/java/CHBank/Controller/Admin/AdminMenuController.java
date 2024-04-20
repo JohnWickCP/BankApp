7e4fc739a5706f898f67d2d0cd8722275b7855d6
JohnWickCP <phonk63utc@gmail.com>
2024-04-20 22:36:07 +0700

    change to AdminMenu

--- src/main/java/CHBank/Controller/Admin/AdminMenuController.java
+++ src/main/java/CHBank/Controller/Admin/AdminMenuController.java
@@ -6,7 +6,11 @@ import javafx.scene.control.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class AdminController implements Initializable {
+public class AdminMenuController implements Initializable {
+    public Button create_client_button;
+    public Button clients_list_button;
+    public Button deposit_button;
+    public Button logout_button;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
