b0c1ecb85ab55b46669e1fb59e4f5be15cb8773e
JohnWickCP <phonk63utc@gmail.com>
2024-04-20 22:31:10 +0700

    feat:  add Admin bar display

--- src/main/java/CHBank/Controller/Admin/AdminController.java
+++ src/main/java/CHBank/Controller/Admin/AdminController.java
@@ -1,4 +1,19 @@
 package CHBank.Controller.Admin;
 
-public class AdminController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+    public Button create_client_button;
+    public Button clients_list_button;
+    public Button deposit_button;
+    public Button logout_button;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
