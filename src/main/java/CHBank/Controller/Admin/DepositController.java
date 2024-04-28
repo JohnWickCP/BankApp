5bfd3b45e7e874e648036884db648468d9774220
JohnWickCP <phonk63utc@gmail.com>
2024-04-29 00:12:50 +0700

    add deposit (admin) menu

---
+++ src/main/java/CHBank/Controller/Admin/DepositController.java
@@ -0,0 +1,22 @@
+package CHBank.Controller.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DepositController implements Initializable {
+    public TextField pAddress_field;
+    public Button search_button;
+    public ListView result_listview;
+    public TextField Amount_filed;
+    public Button deposit_button;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
