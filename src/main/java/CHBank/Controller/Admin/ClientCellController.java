09e46766cae51f515c5ea46b3fa4fff444b8c127
JohnWickCP <phonk63utc@gmail.com>
2024-04-29 22:43:02 +0700

    create Client Cell (Admin)

---
+++ src/main/java/CHBank/Controller/Admin/ClientCellController.java
@@ -0,0 +1,14 @@
+package CHBank.Controller.Admin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ClientCellController {
+    public Label fName_label;
+    public Label lName_label;
+    public Label pAddress_label;
+    public Label ch_acc_label;
+    public Label sv_acc_label;
+    public Label date_label;
+    public Button delete_button;
+}
