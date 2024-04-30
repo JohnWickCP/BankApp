528973de2cb899f63bcc5fa60490452de84b34e7
JohnWickCP <phonk63utc@gmail.com>
2024-05-01 03:42:49 +0700

    feat: Create Client in (Admin)

--- src/main/java/CHBank/Controller/Admin/CreateClientController.java
+++ src/main/java/CHBank/Controller/Admin/CreateClientController.java
@@ -1,5 +1,6 @@
 package CHBank.Controller.Admin;
 
+import CHBank.Models.Model;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -7,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class CreateClientController implements Initializable {
@@ -22,8 +25,89 @@ public class CreateClientController implements Initializable {
     public Button create_client_button;
     public Label error_label;
 
+    private String payeeAddress;
+    private boolean createCheckingAccountFlag= false;
+    private boolean createSavingsAccountFlag = false;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        create_client_button.setOnAction(actionEvent -> createClient());
+        pAddress_box.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                payeeAddress = createPayeeAddress();
+                onCreatePayeeAddress();
+            }
+        });
+        check_acc_box.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                createCheckingAccountFlag = true;
+            }
+        });
+        save_acc_box.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                createSavingsAccountFlag = true;
+            }
+        });
+    }
+
+    private void createClient() {
+        // Create Checking Account
+        if (createCheckingAccountFlag) {
+            createAccount("Checking");
+        }
+
+        // Create Savings Account
+        if (createSavingsAccountFlag) {
+            createAccount("Savings");
+        }
+
+        // Create Client
+         String fName = firstname_filed.getText();
+        String lName = lastname_file.getText();
+        String pass = password_filed.getText();
+        Model.getInstance().getDatabaseDriver().creteClient(fName, lName, payeeAddress, pass, LocalDate.now());
+        error_label.setStyle("-fx-text-fill: blue; -fx-font-size: 1.3em; -fx-font-weight: bold;");
+        error_label.setText("Client Created Successfully");
+        emptyFields();
+    }
+    private void createAccount(String accountType){
+        double balance = Double.parseDouble(check_amount_field.getText());
+
+        // Generate Account Number
+        String firstSection = "3210";
+        String lastSection = Integer.toString((new Random()).nextInt(9999) + 1000);
+        String accountNumber = firstSection + " " + lastSection;
+
+        // Create the checking or saving account
+
+        if(accountType.equals("Checking")){
+            Model.getInstance().getDatabaseDriver().createCheckingAccount(payeeAddress, accountNumber, 10, balance);
+        } else {
+            Model.getInstance().getDatabaseDriver().createSavingsAccount(payeeAddress, accountNumber, 2000, balance);
+        }
+    }
+    private void  onCreatePayeeAddress(){
+        if (firstname_filed.getText() != null && lastname_file.getText() != null) {
+            pAddress_label.setText(payeeAddress);
+        }
+    }
+
+    private String createPayeeAddress(){
+        int id = Model.getInstance().getDatabaseDriver().getLastClientId() + 1;
+        char fChar = Character.toLowerCase(firstname_filed.getText().charAt(0));
+        String address = "@" + fChar + lastname_file.getText() + id;
+        return address.replaceAll("\\s+", "");
+    }
+
+    private void emptyFields(){
+        firstname_filed.clear();
+        lastname_file.clear();
+        password_filed.clear();
+        pAddress_box.setSelected(false);
+        check_acc_box.setSelected(false);
+        check_amount_field.clear();
+        save_acc_box.setSelected(false);
+        save_amount_filed.clear();
 
     }
 }
--- src/main/java/CHBank/Models/DatabaseDriver.java
+++ src/main/java/CHBank/Models/DatabaseDriver.java
@@ -1,6 +1,7 @@
 package CHBank.Models;
 
 import java.sql.*;
+import java.time.LocalDate;
 
 public class DatabaseDriver {
     private Connection connection;
@@ -16,10 +17,37 @@ public class DatabaseDriver {
         }
     }
 
+    public void openConnection() {
+        try {
+            if (this.connection != null && !this.connection.isClosed()) {
+                //System.out.println("Connection is already open.");
+            } else {
+                this.connection = DriverManager.getConnection("jdbc:sqlite:bank.db");
+                //System.out.println("Connection opened successfully.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (this.connection != null && !this.connection.isClosed()) {
+                this.connection.close();
+                //System.out.println("Connection closed successfully.");
+            } else {
+                //System.out.println("Connection is already closed.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     /* Client Section */
 
     public ResultSet getClientData(String pAddress, String pPassword) {
+        openConnection();
         Statement statement;
         ResultSet resultSet = null;
         try {
@@ -27,6 +55,8 @@ public class DatabaseDriver {
             resultSet = statement.executeQuery("SELECT * FROM Clients WHERE PayeeAddress = '"+pAddress+"' AND Password ='"+pPassword+"';");
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            closeConnection();
         }
         return resultSet;
     }
@@ -36,6 +66,7 @@ public class DatabaseDriver {
     /* Admin Section */
 
     public ResultSet getAdminData(String username, String pPassword) {
+        openConnection();
         Statement statement;
         ResultSet resultSet = null;
         try {
@@ -43,11 +74,75 @@ public class DatabaseDriver {
             resultSet = statement.executeQuery("SELECT * FROM Admins WHERE Username = '"+username+"' AND Password ='"+pPassword+"';");
         } catch (Exception e){
             e.printStackTrace();
+        } finally {
+            closeConnection();
         }
         return resultSet;
     }
 
+    public void creteClient(String firstName, String lastName, String address, String password, LocalDate date) {
+        openConnection();
+        Statement statement;
+        try {
+            statement = this.connection.createStatement();
+            statement.executeUpdate("INSERT INTO " +
+                    "Clients(FirstName, LastName, PayeeAddress, Password, Date)" +
+                    "VALUES ('"+firstName+"', '"+lastName+"', '"+address+"', '"+password+"', '"+date.toString()+"');");
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+    }
+
+    public void createCheckingAccount(String owner, String number, double tLimit, double balance){
+        openConnection();
+        try{
+            PreparedStatement statement = this.connection.prepareStatement("INSERT INTO CheckingAccounts (Owner, AccountNumber, TransactionLimit, Balance) VALUES (?, ?, ?, ?)");
+            statement.setString(1, owner);
+            statement.setString(2, number);
+            statement.setDouble(3, tLimit);
+            statement.setDouble(4, balance);
+            statement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+    }
+
+    public void createSavingsAccount(String owner, String number, double wLimit, double balance){
+        openConnection();
+        try{
+            PreparedStatement statement = this.connection.prepareStatement("INSERT INTO SavingsAccounts (Owner, AccountNumber, WithdrawalLimit, Balance) VALUES (?, ?, ?, ?)");
+            statement.setString(1, owner);
+            statement.setString(2, number);
+            statement.setDouble(3, wLimit);
+            statement.setDouble(4, balance);
+            statement.executeUpdate();
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+
+    }
 
     /* Utility Methods */
-
+    public int getLastClientId(){
+        openConnection();
+        Statement statement;
+        ResultSet resultSet ;
+        int id = 0 ;
+        try {
+            statement = this.connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM sqlite_sequence WHERE name = 'Clients';");
+            id = resultSet.getInt("seq");
+        } catch (SQLException e){
+            e.printStackTrace();
+        } finally {
+            closeConnection();
+        }
+        return id;
+    }
 }
