b2484ffae575ef8f4a133a6e7d2cbbe4ba27907b
JohnWickCP <phonk63utc@gmail.com>
2024-05-02 03:43:07 +0700

    feat: Handing Transaction Icon (Client)

--- src/main/java/CHBank/Controller/Client/TransactionCellController.java
+++ src/main/java/CHBank/Controller/Client/TransactionCellController.java
@@ -1,9 +1,11 @@
 package CHBank.Controller.Client;
 
+import CHBank.Models.Model;
 import CHBank.Models.Transaction;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -26,5 +28,16 @@ public class TransactionCellController implements Initializable {
         receiver_label.textProperty().bind(transaction.receiverProperty());
         amount_label.textProperty().bind(transaction.amountProperty().asString());
         trans_date_label.textProperty().bind(transaction.dateProperty().asString());
+        transactionIcon();
+    }
+
+    private void transactionIcon(){
+        if(transaction.senderProperty().get().equals(Model.getInstance().getClient().pAddress().get())){
+            in_icon.setFill(Color.rgb(240, 240, 240));
+            out_icon.setFill(Color.RED);
+        } else {
+            in_icon.setFill(Color.GREEN);
+            out_icon.setFill(Color.rgb(240, 240 ,240));
+        }
     }
 }
